b66f69b4c57ff0f390ee934a043721e8ee25a089
Dejan Obradovic <Dejan.Obradovic@endava.com>
2021-03-19 18:06:49 +0100

    Adding PropTypes to headline component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Headline from "./component/headline";
 
 import "./app.scss";
 
+const tempArr = [
+  {
+    fName: "Joe",
+    lName: "Bloggs",
+    email: "joeblog@gmail.com",
+    age: 24,
+    onlineStatus: true,
+  },
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <section className="main">
-          <Headline header="Posts" desc="Click the button to render posts" />
+          <Headline
+            header="Posts"
+            desc="Click the button to render posts"
+            tempArr={tempArr}
+          />
         </section>
       </div>
     );
