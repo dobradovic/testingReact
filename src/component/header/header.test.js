b1935205280f079b4261de41cc5698f80ef7b2a1
Dejan Obradovic <Dejan.Obradovic@endava.com>
2021-03-19 14:50:05 +0100

    Header Component and Tests

---
+++ src/component/header/header.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+import { findByTestAttr } from "./../../../Utils";
+
+const setUp = (props = {}) => {
+  const component = shallow(<Header {...props} />);
+  return component;
+};
+
+describe("Header Component", () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it("Should render without erros", () => {
+    const wrapper = findByTestAttr(component, "headerComponent");
+    expect(wrapper.length).toBe(1);
+  });
+
+  it("Should render a logo", () => {
+    const wrapper = findByTestAttr(component, "logoIMG");
+    expect(wrapper.length).toBe(1);
+  });
+});
--- src/component/header/index.js
+++ src/component/header/index.js
@@ -4,10 +4,16 @@ import Logo from "./../../assets/graphics/logo.jpg";
 
 const Header = (props) => {
   return (
-    <header>
+    <header data-test="headerComponent">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="Logo" width="120" height="80" />
+          <img
+            data-test="logoIMG"
+            src={Logo}
+            alt="Logo"
+            width="120"
+            height="80"
+          />
         </div>
       </div>
     </header>
